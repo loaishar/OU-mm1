ec1f95d9151cfd113952bcd95efd2de1c43fb829
loaishar <55962133+loaishar@users.noreply.github.com>
2022-11-12 00:18:13 +0200

    commit

---
+++ Triangle1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Triangle1
+{
+ public static void main (String [] args)
+ {
+ Scanner scan = new Scanner (System.in);
+ System.out.println ("This program calculates the area "
+ + "and the perimeter of a given triangle. ");
+ System.out.println ("Please enter the three lengths"
+ + " of the triangle's sides");
+ int a = scan.nextInt();
+ int b = scan.nextInt();
+ int c = scan.nextInt();
+
+
+ 
+ } // end of method main
+} //end of class Triangle
