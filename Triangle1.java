559a626bc3db2bc9968228f1fc549668137962d4
loaishar <55962133+loaishar@users.noreply.github.com>
2022-11-12 19:55:31 +0200

    ff
    fff

--- Triangle1.java
+++ Triangle1.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
-public class Triangle1
-{
- public static void main (String [] args)
- {
-    // Declare an object and initialize with
+
+public class Triangle1 {
+    public static void main(String[] args) {
+        // Declare an object and initialize with
         // predefined standard input object
- Scanner scan = new Scanner(System.in);
- System.out.println ("This program calculates the area "
- + "and the perimeter of a given triangle. ");
- System.out.println ("Please enter the three lengths"
- + " of the triangle's sides");
- int a = scan.nextInt();//input first lengths
- int b = scan.nextInt();//input second lengths
- int c = scan.nextInt();//input third lengths
+        Scanner scan = new Scanner(System.in);
+        System.out.println("This program calculates the area "
+                + "and the perimeter of a given triangle. ");
+        System.out.println("Please enter the three lengths"
+                + " of the triangle's sides");
+        int a = scan.nextInt();// input first lengths
+        int b = scan.nextInt();// input second lengths
+        int c = scan.nextInt();// input third lengths
 
- double perimeter = a +b +c;//calculate perimeter
- double s = perimeter / 2; //half the circumference.
- double area = Math.sqrt(s*((s-a)*(s-b)*(s-c))); // using Heron's formule
- System.out.println( "The lengths of the triangle sides are:"+a+","+b+","+c);
- System.out.println("The perimeter of the triangle is:"+(int)perimeter);
-System.out.println("The area of the triangle is: "+area);
+        double perimeter = a + b + c;// calculate perimeter
+        double s = perimeter / 2; // half the circumference.
+        double area = Math.sqrt(s * ((s - a) * (s - b) * (s - c))); // using Heron's formule
+        System.out.println("The lengths of the triangle sides are:" + a + "," + b + "," + c);
+        System.out.println("The perimeter of the triangle is:" + (int) perimeter);
+        System.out.println("The area of the triangle is: " + area);
 
-
- 
- } // end of method main
-} //end of class Triangle
+    } // end of method main
+} // end of class Triangle
--- Triangle2.java
+++ Triangle2.java
@@ -9,9 +9,9 @@ public class Triangle2 {
                 + "and the perimeter of a given triangle. ");
         System.out.println("Please enter the three lengths"
                 + " of the triangle's sides");
-        int a = scan.nextInt();//input first lengths
-        int b = scan.nextInt();//input second lengths
-        int c = scan.nextInt();//input third lengths
+        int a = scan.nextInt();// input first lengths
+        int b = scan.nextInt();// input second lengths
+        int c = scan.nextInt();// input third lengths
         if (a + b <= c || a + c <= b || b + c <= a) {
             System.out.println("The numbers: " + a + ", " + b + " and " + c + " cannot represent a triangle");
         } else {// Check for equilateral triangle
@@ -22,11 +22,11 @@ public class Triangle2 {
             else if (a == b || b == c || c == a)
                 System.out.println("The numbers: " + a + ", " + b + " and " + c + " represent an isosceles triangle");
             // Check for right-angle triangle
-            else  if((a*a)+(b*b)==(c*c) || (a*a)+(c*c)==(b*b) || (b*b)+(c*c)==(a*a) )
+            else if ((a * a) + (b * b) == (c * c) || (a * a) + (c * c) == (b * b) || (b * b) + (c * c) == (a * a))
                 System.out.println("The numbers: " + a + ", " + b + " and " + c + " represent a right-angle triangle");
-                else{// Otherwise common triangle
-                    System.out.println("The numbers: " + a + ", " + b + " and " + c + " represent a common triangle");
-                }
+            else {// Otherwise common triangle
+                System.out.println("The numbers: " + a + ", " + b + " and " + c + " represent a common triangle");
+            }
         }
     } // end of method main
 } // end of class Triangle2
